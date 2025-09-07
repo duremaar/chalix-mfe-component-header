6303f97b509197c9c67ebcff2d2fb1623d25b26a
dungdl <dungdl0298@gmail.com>
2025-09-07 15:54:59 +0700

    Fix dist index.js to exclude problematic StudioHeader

--- dist/index.js
+++ dist/index.js
@@ -2,7 +2,6 @@ import Header from './Header';
 import ChalixHeader from './ChalixHeader';
 import LearningHeader from './learning-header/LearningHeader';
 import messages from './i18n/index';
-import StudioHeader from './studio-header';
-export { ChalixHeader, LearningHeader, messages, StudioHeader };
+export { ChalixHeader, LearningHeader, messages };
 export default Header;
 //# sourceMappingURL=index.js.map
