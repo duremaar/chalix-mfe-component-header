b30edc8039ab9466ac26003681d84ece66e6f1b7
dungdl <dungdl0298@gmail.com>
2025-09-07 16:14:06 +0700

    Add StudioHeader export to dist/index.js

--- dist/index.js
+++ dist/index.js
@@ -2,6 +2,7 @@ import Header from './Header';
 import ChalixHeader from './ChalixHeader';
 import LearningHeader from './learning-header/LearningHeader';
 import messages from './i18n/index';
-export { ChalixHeader, LearningHeader, messages };
+import StudioHeader from './studio-header';
+export { ChalixHeader, LearningHeader, messages, StudioHeader };
 export default Header;
 //# sourceMappingURL=index.js.map
