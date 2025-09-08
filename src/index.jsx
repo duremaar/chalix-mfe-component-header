55337c3cce8d443704993247e3bc1062491bdc6b
dungdl <dungdl0298@gmail.com>
2025-09-09 04:37:15 +0700

    clean: Remove source files and dev dependencies from release branch
    - Remove src/ directory with TypeScript source files that caused webpack parsing errors
    - Remove development configuration files
    - Keep only package.json, .gitignore, and dist/ for clean npm package
    - Fixes 'Unexpected token' error when Tutor builds MFE with @chalix/frontend-component-header

--- src/index.jsx
+++
@@ -1,9 +0,0 @@
-import Header from './Header';
-import ChalixHeader from './ChalixHeader';
-import LearningHeader from './learning-header/LearningHeader';
-import messages from './i18n/index';
-import StudioHeader from './studio-header';
-
-export { ChalixHeader, LearningHeader, messages, StudioHeader };
-
-export default Header;
